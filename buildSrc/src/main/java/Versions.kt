45e102bb1d4553635d3e7ca216b9a25d8dd79e79
Hyun Woo Lee <l2hyunwoo@gmail.com>
2021-03-31 16:45:10 +0900

    [CHORE] : Kotlin, Gradle 버전 변경
    - Kotlin: 1.4.31 ->  1.4.32
    - Gradle: 4.1.2 -> 4.1.3
    
    issue track: #24

--- buildSrc/src/main/java/Versions.kt
+++ buildSrc/src/main/java/Versions.kt
@@ -1,7 +1,7 @@
 import org.gradle.api.JavaVersion
 
 object Versions {
-    const val kotlin_version = "1.4.31"
+    const val kotlin_version = "1.4.32"
     const val core_ktx_version = "1.3.2"
     const val appcompat_version = "1.2.0"
     const val material_design_version = "1.3.0"
@@ -9,7 +9,7 @@ object Versions {
     const val hilt_version = "2.33-beta"
     const val nav_verion = "2.3.3"
 
-    const val gradle_version = "4.1.2"
+    const val gradle_version = "4.1.3"
 
     const val junit_version = "4.13.2"
     const val espresso_version = "3.3.0"
